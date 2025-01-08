a8f017938f40c2cb7d97e231be8c379355d13441
Dev <devitaliya22@gmail.com>
2025-01-08 19:36:00 +0530

    Added tab feature in Editor

--- src/components/editor/Tiptap.tsx
+++ src/components/editor/Tiptap.tsx
@@ -3,36 +3,48 @@ import StarterKit from "@tiptap/starter-kit";
 import Underline from "@tiptap/extension-underline";
 import TextEditorMenuBar from "./TextEditorMenu";
 
-
 type TextEditorProps = {
-    onChange: (content: string) => void;
-    initialContent?: string; 
-  };
-
+  onChange: (content: string) => void;
+  initialContent?: string;
+};
 
 export default function RichTextEditor({
-    onChange,
-    initialContent,
-  }: TextEditorProps) {
+  onChange,
+  initialContent,
+}: TextEditorProps) {
+  const editor = useEditor({
+    extensions: [StarterKit, Underline],
+    content: initialContent,
+    onUpdate: ({ editor }) => {
+      onChange(editor.getHTML());
+    },
+    editorProps: {
+      attributes: {
+        class:
+          "cursor-text rounded-md border-2 shadow p-5 ring-offset-background focus-within:outline-none min-h-[50vh] sm:min-h-[70vh]",
+      },
+      handleKeyDown: (view, event) => {
+        if (event.key === 'Tab') {
+          event.preventDefault(); 
+          const { state, dispatch } = view;
+          const { selection } = state;
+          const { from, to } = selection;
+          const spaces = '    ';
+          const transaction = state.tr.insertText(spaces, from, to);
 
-    const editor = useEditor({
-        extensions: [StarterKit, Underline],
-        content: initialContent,
-        onUpdate : ({editor}) => {
-            onChange(editor.getHTML())
-        },
-        editorProps: {
-            attributes: {
-                class: "cursor-text rounded-md border-2 shadow p-5 ring-offset-background focus-within:outline-none min-h-[50vh] sm:min-h-[70vh]"
+          dispatch(transaction);
+          return true;
+        }
+        return false;
+      },
+    },
+    immediatelyRender: false,
+  });
 
-            }
-        },
-        immediatelyRender: false
-    })
   return (
     <div className="h-full">
       <TextEditorMenuBar editor={editor} />
-        <EditorContent editor={editor} className="h-full"/>
+      <EditorContent editor={editor} className="h-full" />
     </div>
-  )
+  );
 }
